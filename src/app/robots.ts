acd7cf72947790b9d1647c984d3cf508ca8d045c
Sulton <sultonalidrus5@gmail.com>
2024-12-27 03:36:52 +0700

    ref : robots and sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: `${process.env.NEXT_PUBLIC_URL}/sitemap.xml`,
+    sitemap: `${process.env.VERCEL_URL}/sitemap.xml`,
   };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,13 +3,13 @@ import { type MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: process.env.NEXT_PUBLIC_URL ?? "/",
+      url: process.env.VERCEL_URL ?? "/",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
       alternates: {
         languages: {
-          pl: `${process.env.NEXT_PUBLIC_URL}/pl`,
+          pl: `${process.env.VERCEL_URL}/pl`,
         },
       },
     },
